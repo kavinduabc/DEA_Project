5b11e769acacb3de621739f532560e59c18486e9
kavinduabc <piumalranepura2002@gmail.com>
2026-01-21 15:07:57 +0530

    DEA base project

---
+++ open_learnig_platform/src/main/java/com/open_learnig_platform/OpenLearnigPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.open_learnig_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenLearnigPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenLearnigPlatformApplication.class, args);
+    }
+
+}
---
+++ open_learnig_platform/src/test/java/com/open_learnig_platform/OpenLearnigPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open_learnig_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenLearnigPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
