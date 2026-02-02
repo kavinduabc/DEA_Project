9fa590f397e9cf20c0a68bfed91d05e0975da8a5
kavinduabc <piumalranepura2002@gmail.com>
2026-02-02 23:19:07 +0530

    feate(user-profile):implement JwtService and JWTAuthFilter for authentication

---
+++ backend/profile-service/src/main/java/com/opentuter/userservice/jwt/JwtAuthFilter.java
@@ -0,0 +1,11 @@
+package com.opentuter.userservice.jwt;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+
+}
---
+++ backend/profile-service/src/main/java/com/opentuter/userservice/service/JwtService.java
@@ -0,0 +1,61 @@
+package com.opentuter.userservice.service;
+
+
+import com.opentuter.userservice.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key
+import java.util.Date;
+
+@Component
+public class JwtService {
+
+    // implement the secret key use for generate the jwt token
+    private static final String secret = "my_super_secure_jwt_secret_key_which_is_long_enough_256bits";
+
+    // implement  function for generate signing key from secret string
+    private Key getKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    //*
+    // implement function for generate jwt token for each user
+    // generate this token include in all user details in the token and token expire within one day*/
+    public String generateToken(User user)
+    {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role" , user.getRole())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 864000))
+                .signWith(getKey(), signatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //implement function for extract subject from jwt
+    public String extractEmail(String token)
+    {
+        return extractAllClaims(token).getSubject();
+    }
+
+    //implement function for extract role claim from jet
+    public String extractRole(String toke)
+    {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    //implement function for parse and validate jwt and return
+    private Claims extractAllClaims(String token)
+    {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+
+}
