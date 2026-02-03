2cf040d502867390edd61bd6e81fbf711508636d
kavinduabc <piumalranepura2002@gmail.com>
2026-02-03 09:32:01 +0530

    feate(profile-service):implement methods to generate jwi token and authorization process done

--- backend/profile-service/src/main/java/com/opentuter/userservice/config/SecurityConfig.java
+++ backend/profile-service/src/main/java/com/opentuter/userservice/config/SecurityConfig.java
@@ -1,17 +1,61 @@
 package com.opentuter.userservice.config;
 
 
+import com.opentuter.userservice.jwt.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    public PasswordEncoder passwordEncoder()
+//    {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    //*
+    // implement functions for authentication process
+    // admin what to do and user what to do etc*/
     @Bean
-    public PasswordEncoder passwordEncoder()
-    {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.csrf(csrf ->csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                )
+                .sessionManagement(sess ->
+                        sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, EmailPasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config
+    ) throws Exception{
+        return config.getAuthenticationManager();
     }
 }
--- backend/profile-service/src/main/java/com/opentuter/userservice/jwt/JwtAuthFilter.java
+++ backend/profile-service/src/main/java/com/opentuter/userservice/jwt/JwtAuthFilter.java
@@ -1,11 +1,76 @@
 package com.opentuter.userservice.jwt;
 
 
+import com.opentuter.userservice.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyles.header;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
 
+    private final JwtService jwtService;
+
+
+    public JwtAuthFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    //*
+    // implement funtcion for spring security which requests to SKIP filtering
+    // Authentication end points*/
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request)
+    {
+        return request.getServletPath().startsWith("/api/auth");
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException{
+        //implemnt methos for continue request without authentication
+        if(header == null || !header.startsWith("Bearer")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // method for remove "Bearer" and get only JWT token
+        String token = header.substring(7);
+
+        //method for extract email and role from jwt
+        String email = jwtService.extractEmail(token);
+        String role = jwtService.extractRole(token);
+
+        //implement method for convert role into spring security authority
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
+
+        //*
+        // implement method for create authentication object
+        // according to that user is now authenticated*/
+        EmailPasswordAuthenticationToken auth = new EmailPasswordAuthenticationToken(
+                email,
+                null,
+                authorities
+        );
+
+        //implement method for store authentication is securitycontext and continue request processing
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        filterChain.doFilter(request, response);
+
+    }
 }
