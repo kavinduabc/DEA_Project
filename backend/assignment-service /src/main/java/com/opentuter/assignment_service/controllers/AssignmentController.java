57c1c424dbeec37f1e87b799bf32397ffc7623f1
Sandinu weerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2026-02-06 20:52:59 +0530

    fix: Remove accidental duplicate folder

--- backend/assignment-service /src/main/java/com/opentuter/assignment_service/controllers/AssignmentController.java
+++
@@ -1,9 +0,0 @@
-package com.opentuter.assignment_service.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class assignmentController {
-    @RequestMapping
-}
